cdecd2fb459e9dcf76354dee8c6dddb5eafd4b6f
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2025-11-29 07:30:22 +0530

    Introductory Part

--- CSS/Introductory Part/script.js
+++ CSS/Introductory Part/script.js
@@ -1,1 +1,8 @@
 alert("GOAT Is Arriving");
+console.log("GOAT Is Arriving");
+document.write("GOAT Is Arriving");
+document.getElementById("demo").innerHTML = "GOAT Is Arriving";
+document.querySelector("#demo").innerHTML = "GOAT Is Arriving";
+window.onload = function() {
+    document.getElementById("demo").innerHTML = "GOAT Is Arriving";
+}
